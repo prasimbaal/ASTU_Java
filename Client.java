41c971170f6d389bac20e57a8a2bcc24446e4b39
prasimbaal <prasimbaal@gmail.com>
2024-01-02 20:03:11 +0300

    p2p implemented

--- Client.java
+++ Client.java
@@ -1,20 +1,31 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import javax.swing.*;
 
 public class Client{
-	
+
 	public static void main(String[] args) throws IOException{
 		Scanner input = new Scanner(System.in);
 		
 		Socket client = new Socket("localhost", 4999);
-		System.out.print("Client message: " );
-
-		String message = input.nextLine();
+		System.out.println("Connected to server");
 		
+		//client
+		System.out.print("Client1 sends: ");
+		String message = input.nextLine();
+
 		PrintWriter out = new PrintWriter(client.getOutputStream());
 
 		out.println(message);
 		out.flush();
+
+		//server
+		InputStreamReader in =  new InputStreamReader(client.getInputStream());
+
+		BufferedReader bf = new BufferedReader(in);
+		String str = bf.readLine();
+		System.out.println("received from client2: "+str);
+
 	}
 }
--- Server.java
+++ Server.java
@@ -1,19 +1,32 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Server {
 	public static void main(String[] args) throws IOException{
+
+		Scanner input = new Scanner(System.in);
 		ServerSocket server = new ServerSocket(4999);
-		System.out.print("Server: " + server);
+		// System.out.print("Server: " + server);
 		Socket client = server.accept();
 
-		System.out.print("Client connected ");
+		System.out.println("Client connected ");
 
+		//server
 		InputStreamReader in =  new InputStreamReader(client.getInputStream());
 
 		BufferedReader bf = new BufferedReader(in);
 		String str = bf.readLine();
-		System.out.println(str);
+		System.out.println("Received from client1: " + str);
+
+		//client
+		System.out.print("Client2 says: ");
+		String message = input.nextLine();
+		PrintWriter out = new PrintWriter(client.getOutputStream());
+
+		out.println(message);
+		out.flush();
 	}
 
 }
