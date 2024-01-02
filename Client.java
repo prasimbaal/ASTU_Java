6f022b636d170eb97692010f99035ee829f3aa2c
prasimbaal <prasimbaal@gmail.com>
2024-01-02 18:48:38 +0300

    message input feautre added

--- Client.java
+++ Client.java
@@ -1,10 +1,20 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Client{
 	
 	public static void main(String[] args) throws IOException{
-		Socket client = new Socket("localhost", 8888);
-		System.out.println("Client: "  + client);
+		Scanner input = new Scanner(System.in);
+		
+		Socket client = new Socket("localhost", 4999);
+		System.out.print("Client message: " );
+
+		String message = input.nextLine();
+		
+		PrintWriter out = new PrintWriter(client.getOutputStream());
+
+		out.println(message);
+		out.flush();
 	}
 }
--- Server.java
+++ Server.java
@@ -3,10 +3,17 @@ import java.io.*;
 
 public class Server {
 	public static void main(String[] args) throws IOException{
-		ServerSocket server = new ServerSocket(8888);
+		ServerSocket server = new ServerSocket(4999);
 		System.out.print("Server: " + server);
 		Socket client = server.accept();
+
 		System.out.print("Client connected ");
+
+		InputStreamReader in =  new InputStreamReader(client.getInputStream());
+
+		BufferedReader bf = new BufferedReader(in);
+		String str = bf.readLine();
+		System.out.println(str);
 	}
 
 }
